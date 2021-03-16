c8c668237df65d0187c1feddc06ad4afc5c7f1f9
rodrigotuna <rodrigotuna.andrade@gmail.com>
2021-03-16 23:27:25 +0000

    remover comentarios desnecessarios

--- src/xmod.c
+++ src/xmod.c
@@ -358,8 +358,7 @@ int main(int nargs, char *args[]) {
 		writeLog(getpid(), PROC_CREAT, logMsg, &processData);
 	}
 
-	if (nargs > 2){
-
+	if (nargs > 3){
 		u_int8_t flags = getFlags(nargs, args);
 		char *modeStr = args[nargs - 2];
 

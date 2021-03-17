e63ea59a8a3224abb749999e4b841d287f1ecf3a
Bia <beatrizaguiar.up@gmail.com>
2021-03-17 22:20:50 +0000

    tested

--- src/xmod.c
+++ src/xmod.c
@@ -18,15 +18,18 @@
 
 XmodData processData;
 
-void stopProcesses(){
+void stopProcesses()
+{
 	kill(0, SIGCONT);
 	sleep(1);
 	printf("Do you want to continue? (y or n)\n");
 	char input;
-	do{
+	do
+	{
 		scanf("%c", &input);
 
-		if (input == 'n'){
+		if (input == 'n')
+		{
 
 			writeLog(getpid(), SIGNAL_SENT, "SIGKILL : 0", &processData);
 			kill(0, SIGKILL);
@@ -35,20 +38,22 @@ void stopProcesses(){
 	} while (input != 'y' && input != 'n');
 
 	writeLog(getpid(), SIGNAL_SENT, "SIGCONT: 0", &processData);
-	
 }
 
-void contHandler(int sig){}
+void contHandler(int sig) {}
 
-void genericSignalHandler(int signal) {
-	if (getenv("LOG_FILENAME") != NULL) {
+void genericSignalHandler(int signal)
+{
+	if (getenv("LOG_FILENAME") != NULL)
+	{
 		char buffer[20];
 		snprintf(buffer, sizeof(buffer), "%d", signal);
 		writeLog(getpid(), SIGNAL_RECV, buffer, &processData);
 	}
 }
 
-void receiveSignal() {
+void receiveSignal()
+{
 	struct sigaction new, sigintAction;
 	sigset_t smask;
 
@@ -60,7 +65,7 @@ void receiveSignal() {
 	new.sa_mask = smask;
 	new.sa_flags = 0;
 
-	sigintAction.sa_handler =  sigintHandler;
+	sigintAction.sa_handler = sigintHandler;
 	sigintAction.sa_mask = smask;
 	sigintAction.sa_flags = 0;
 	if (sigaction(SIGINT, &sigintAction, NULL) == -1)
@@ -86,14 +91,18 @@ void receiveSignal() {
 		perror("sigaction");
 }
 
-void sigintHandler(int sig){
+void sigintHandler(int sig)
+{
 
 	printf("%d ; %s ; %d ; %d\n", getpid(), processData.currentDirectory, processData.nTotal, processData.nModif);
 	writeLog(getpid(), SIGNAL_RECV, "SIGINT", &processData);
 
-	if (getpid() == getpgrp()){
+	if (getpid() == getpgrp())
+	{
 		stopProcesses();
-	} else{
+	}
+	else
+	{
 		struct sigaction new;
 		sigset_t smask;
 
@@ -108,7 +117,7 @@ void sigintHandler(int sig){
 			perror("sigaction");
 		//Wait for signal from first process
 		//signal(SIGCONT, contHandler);
-		
+
 		sigset_t wset;
 		sigemptyset(&wset);
 		sigaddset(&wset, SIGCONT);
@@ -127,7 +136,8 @@ void sigintHandler(int sig){
  * @param flags 
  * @return int 
  */
-int xmod(char *path, char *modeStr, u_int8_t flags, mode_t previousMode) {
+int xmod(char *path, char *modeStr, u_int8_t flags, mode_t previousMode)
+{
 	processData.nTotal++;
 	char first = modeStr[0];
 	mode_t mode;
@@ -137,13 +147,18 @@ int xmod(char *path, char *modeStr, u_int8_t flags, mode_t previousMode) {
 	sleep(1);
 	previousMode &= ALL_PERMS;
 
-	if (first == '0'){   //numerical mode
+	if (first == '0')
+	{ //numerical mode
 		mode = strtol(modeStr, 0, 8);
-	} else if (first == 'u' || first == 'g' || first == 'o' || first == 'a'){
+	}
+	else if (first == 'u' || first == 'g' || first == 'o' || first == 'a')
+	{
 		//Symbolic mode
 		mode = previousMode;
 		symbolicXmod(modeStr, &mode);
-	} else {
+	}
+	else
+	{
 		fprintf(stderr, "xmod: Invalid type of user - %c\n", first);
 		return -1;
 	}
@@ -154,26 +169,30 @@ int xmod(char *path, char *modeStr, u_int8_t flags, mode_t previousMode) {
 	getSymbolic(previousMode, previousModeS);
 	getSymbolic(mode, modeS);
 
-
-	if (chmod(path, mode) != 0) {
+	if (chmod(path, mode) != 0)
+	{
 		fprintf(stderr, "failed to change mode of '%s' from %04o (%s) to %04o (%s)\n", path, previousMode, previousModeS, mode, modeS);
 		return -1;
 	}
 
 	if (previousMode != mode)
 		processData.nModif = processData.nModif + 1;
-	
-	
-	if (flags & (CHANG_FLAG | VERB_FLAG)){
-		
-		if (previousMode != mode){
+
+	if (flags & (CHANG_FLAG | VERB_FLAG))
+	{
+
+		if (previousMode != mode)
+		{
 			printf("mode of '%s' changed from %04o (%s) to %04o (%s)\n", path, previousMode, previousModeS, mode, modeS);
-		} else if (flags & VERB_FLAG){
+		}
+		else if (flags & VERB_FLAG)
+		{
 			printf("mode of '%s' retained as %04o (%s)\n", path, mode, modeS);
 		}
 	}
 
-	if (previousMode != mode){
+	if (previousMode != mode)
+	{
 		char logMsg[500];
 		snprintf(logMsg, sizeof(logMsg), "%s : %04o : %04o", path, previousMode, mode);
 		writeLog(getpid(), FILE_MODF, logMsg, &processData);
@@ -182,32 +201,37 @@ int xmod(char *path, char *modeStr, u_int8_t flags, mode_t previousMode) {
 	return 0;
 }
 
-int symbolicXmod(char *modeStr, mode_t *newMode){
+int symbolicXmod(char *modeStr, mode_t *newMode)
+{
 
 	char operator= modeStr[1];
 	mode_t mode = 0, previousMode = *newMode;
-	
+
 	//Permissions start at modeStr[2]
 	int i = SYMB_PERM_START;
 	bool r_flag = false;
 	bool w_flag = false;
 	bool x_flag = false;
 	//Get permissions
-	while (modeStr[i] != 0){
+	while (modeStr[i] != 0)
+	{
 		switch (modeStr[i++])
 		{
 		case 'r':
-			if(r_flag) break;
+			if (r_flag)
+				break;
 			mode += READ_MASK;
 			r_flag = true;
 			break;
 		case 'w':
-			if(w_flag) break;
+			if (w_flag)
+				break;
 			mode += WRITE_MASK;
 			w_flag = true;
 			break;
 		case 'x':
-			if(x_flag) break;
+			if (x_flag)
+				break;
 			mode += EXE_MASK;
 			x_flag = true;
 			break;
@@ -242,7 +266,7 @@ int symbolicXmod(char *modeStr, mode_t *newMode){
 		return -1;
 	}
 
-	mask += FILE_MASK;  //regular files have bit 1 at pos5
+	mask += FILE_MASK; //regular files have bit 1 at pos5
 
 	//Get new mode
 	switch (operator)
@@ -264,41 +288,48 @@ int symbolicXmod(char *modeStr, mode_t *newMode){
 	return 0;
 }
 
-void goThroughDirectory(char *path, int nargs, char *args[], u_int8_t flags){
+void goThroughDirectory(char *path, int nargs, char *args[], u_int8_t flags)
+{
 
 	bool hasLog = getenv("LOG_FILENAME") != NULL;
 	char logMsg[1024];
 	char *modeStr = args[nargs - 2];
-	
+
 	DIR *dir;
 	struct dirent *dent;
 	dir = opendir(processData.currentDirectory);
 
-	if (dir != NULL) {
-		while ((dent = readdir(dir)) != NULL) {
+	if (dir != NULL)
+	{
+		while ((dent = readdir(dir)) != NULL)
+		{
 
 			if (!strcmp(dent->d_name, ".") || !strcmp(dent->d_name, ".."))
 				continue;
-			
+
 			char nextPath[500];
 			snprintf(nextPath, sizeof(nextPath), "%s/%s", processData.currentDirectory, dent->d_name);
 			struct stat nextFileInfo;
 
 			int statErrorCode = stat(nextPath, &nextFileInfo);
 
-			if (statErrorCode != 0){
-				if (statErrorCode == EACCES) fprintf(stderr, "xmod: cannot read directory '%s': Permission denied\n", nextPath);
-				else fprintf(stderr, "xmod: cannot access  '%s': No such file or directory\n", nextPath);
+			if (statErrorCode != 0)
+			{
+				if (statErrorCode == EACCES)
+					fprintf(stderr, "xmod: cannot read directory '%s': Permission denied\n", nextPath);
+				else
+					fprintf(stderr, "xmod: cannot access  '%s': No such file or directory\n", nextPath);
 				continue;
 			}
 
-				
 			xmod(nextPath, modeStr, flags, nextFileInfo.st_mode);
 
-			if (S_ISDIR(nextFileInfo.st_mode)) {
+			if (S_ISDIR(nextFileInfo.st_mode))
+			{
 				int id = fork();
 
-				switch (id){
+				switch (id)
+				{
 				case 0:
 					snprintf(args[nargs - 1], sizeof(nextPath), "%s", nextPath);
 					execvp("./xmod", args);
@@ -306,9 +337,10 @@ void goThroughDirectory(char *path, int nargs, char *args[], u_int8_t flags){
 				case -1:
 					return;
 				default:
-					if (hasLog) {
+					if (hasLog)
+					{
 						char logMsgArgs[1024];
-						getArgStr(nargs, args,logMsgArgs);
+						getArgStr(nargs, args, logMsgArgs);
 						snprintf(logMsg, sizeof(logMsgArgs) + sizeof(dent->d_name), "%s/%s", logMsgArgs, dent->d_name);
 						writeLog(getpid(), PROC_CREAT, logMsg, &processData);
 					}
@@ -321,28 +353,33 @@ void goThroughDirectory(char *path, int nargs, char *args[], u_int8_t flags){
 		pid_t wpid;
 		int status = 0;
 		//Parent must wait for all children to finnish process
-		while ((wpid = wait(&status)) > 0){
+		while ((wpid = wait(&status)) > 0)
+		{
 			snprintf(logMsg, sizeof(logMsg), "%d", status);
-			if(hasLog)
+			if (hasLog)
 				writeLog(wpid, PROC_EXIT, logMsg, &processData);
 		}
 	}
 }
 
-int main(int nargs, char *args[]) {
+int main(int nargs, char *args[])
+{
 	processData.nTotal = 0;
 	processData.nModif = 0;
 	bool hasLog = getenv("LOG_FILENAME") != NULL;
-	char *buffer = (char *) malloc(sizeof(char) * 50);
+	char *buffer = (char *)malloc(sizeof(char) * 50);
 
-	//Check if is first born process 
-	if ((getpgrp() == getpid()) && hasLog) {
+	//Check if is first born process
+	if ((getpgrp() == getpid()) && hasLog)
+	{
 		gettimeofday(&processData.startTime, NULL);
-    	snprintf(buffer, sizeof(char) * 50, "xmodStartTime=%lu %lu", processData.startTime.tv_sec, processData.startTime.tv_usec);
-    	putenv(buffer);
+		snprintf(buffer, sizeof(char) * 50, "xmodStartTime=%lu %lu", processData.startTime.tv_sec, processData.startTime.tv_usec);
+		putenv(buffer);
 		initLog(&processData);
-	} else if (hasLog) { 
-		//read first process starting time 
+	}
+	else if (hasLog)
+	{
+		//read first process starting time
 		char *startTimeStr = getenv("xmodStartTime");
 		struct timeval startTime;
 
@@ -350,15 +387,16 @@ int main(int nargs, char *args[]) {
 		processData.startTime = startTime;
 	}
 
-	char *logMsg = (char *)malloc(sizeof(char) * 500);
+	char logMsg[1024];
+	if (hasLog)
+	{
 
-	if (hasLog){
-
-		getArgStr( nargs, args,logMsg);
+		getArgStr(nargs, args, logMsg);
 		writeLog(getpid(), PROC_CREAT, logMsg, &processData);
 	}
 
-	if (nargs > 2){
+	if (nargs > 2)
+	{
 		u_int8_t flags = getFlags(nargs, args);
 		char *modeStr = args[nargs - 2];
 
@@ -368,7 +406,8 @@ int main(int nargs, char *args[]) {
 		struct stat fileInfo;
 		int statErrorCode = stat(processData.currentDirectory, &fileInfo);
 
-		if (statErrorCode != 0){
+		if (statErrorCode != 0)
+		{
 			if (statErrorCode == EACCES)
 				fprintf(stderr, "xmod: cannot read directory '%s': Permission denied\n", processData.currentDirectory);
 			else
@@ -377,16 +416,17 @@ int main(int nargs, char *args[]) {
 		}
 
 		//Check if first born process
-		if (getpgrp() == getpid()){
+		if (getpgrp() == getpid())
+		{
 			xmod(processData.currentDirectory, modeStr, flags, fileInfo.st_mode);
 		}
-		if (flags & REC_FLAG && S_ISDIR(fileInfo.st_mode)) {
+		if (flags & REC_FLAG && S_ISDIR(fileInfo.st_mode))
+		{
 			goThroughDirectory(path, nargs, args, flags);
 		}
 	}
 
-	free(logMsg);
-	free(buffer);
+		free(buffer);
 
 	return 0;
 }
